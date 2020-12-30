5ced5b895d3fd666300402f438c3d0f02ae864a2
brewokter <leonardwang40@yahoo.com>
2020-12-30 14:43:40 +0700

    coba gambar

--- app/src/main/java/com/example/petakecelakaansepeda/MainActivity.java
+++ app/src/main/java/com/example/petakecelakaansepeda/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.petakecelakaansepeda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
 }
